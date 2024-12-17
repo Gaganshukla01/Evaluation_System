8b106e64d87819cf0d80108f2e9943b2a1897442
Arya Jain <aryajain2491@gmail.com>
2024-12-17 14:44:54 +0530

    displayEmployeedetails

--- server/src/model/employee.model.js
+++ server/src/model/employee.model.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
 const EmployeeSchema = new mongoose.Schema({
-  id: {
+  empId: {
     type: Number,
-    unique: true,
     required: true,
   },
   name: {

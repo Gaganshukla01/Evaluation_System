3fd875e1a03a725c9c6852af73b284b399ba1da2
Thomas <thomasshibu94@yash.com>
2024-12-17 14:47:37 +0530

    Update

--- server/src/controllers/adminController.js
+++ server/src/controllers/adminController.js
@@ -2,8 +2,18 @@ const Employee = require("../model/employee.model");
 
 const addEmployee = async (req, res) => {
     try {
-        const { id, name, email, designation } = req.body;
-        const newEmployee = new Employee({ id, name, email, designation });
+        const { empId, name, email, designation } = req.body;
+        if (!empId) {
+            return res.status(400).send("empId is required");
+        }
+
+        const existingEmployee = await Employee.findOne({ empId });
+        if (existingEmployee) {
+            return res.status(400).send("Employee with this empId already exists");
+        }
+
+        const newEmployee = new Employee({ empId, name, email, designation });
+        console.log(newEmployee);
         await newEmployee.save();
         res.status(201).send('Employee Added Successfully');
     } catch (error) {
--- server/src/model/employee.model.js
+++ server/src/model/employee.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const EmployeeSchema = new mongoose.Schema({
-  id: {
+  empId: {
     type: Number,
     unique: true,
     required: true,
